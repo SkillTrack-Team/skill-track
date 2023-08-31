45132e3741ada0bf9c27214099a479308d6f8b8c
Kareem Saleh <kareemhs@gmail.com>
2023-08-30 21:51:44 -0400

    Setup frontend pages and modal for applications

---
+++ my-ts-app/src/components/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login(): JSX.Element {
+  return (
+    <div className="Login">
+      <h1>Login Page</h1>
+    </div>
+  );
+}
---
+++ my-ts-app/src/components/Welcome.tsx
@@ -0,0 +1,10 @@
+import Navbar from './Navbar';
+
+export default function Welcome(): JSX.Element {
+  return (
+    <div className="Welcome">
+      <Navbar />
+      <h1>Welcome Page</h1>
+    </div>
+  );
+}
