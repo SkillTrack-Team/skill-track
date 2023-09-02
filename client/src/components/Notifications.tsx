22e78a6563b0113fed4377438930f70772a6e11d
Kareem Saleh <kareemhs@gmail.com>
2023-09-02 12:42:28 -0400

    Add boilerplate for notifications page and account page

--- client/src/components/Notifications.tsx
+++ client/src/components/Notifications.tsx
@@ -1,10 +1,50 @@
+import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+} from '@mui/material';
 import Navbar from './Navbar';
 
 export default function Notifications(): JSX.Element {
+  const friendRequests = [
+    { id: 1, text: 'Alex sent you a friend request.' },
+    { id: 2, text: 'Jess sent you a friend request.' },
+    { id: 3, text: 'Charles sent you a friend request.' },
+  ];
+
   return (
     <div className="Notifications">
       <Navbar />
-      <h1>Notifications Page</h1>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Friend Request</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {friendRequests.map((request) => (
+              <TableRow key={request.id}>
+                <TableCell align="left">{request.text}</TableCell>
+                <TableCell align="right">
+                  <Button variant="contained" color="success">
+                    Accept
+                  </Button>
+                  <Button variant="contained" color="error">
+                    Decline
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
